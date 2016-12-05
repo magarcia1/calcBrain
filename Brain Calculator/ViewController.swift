e951314458c3e9a074f5a8b62dc0d77cd593c7e5
Miguel Garcia <miguelgarcia@miguels-macbook-pro.local>
2016-12-05 15:57:55 -0700

    Initial Commit

---
+++ Brain Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Brain Calculator
+//
+//  Created by Miguel Garcia on 12/5/16.
+//  Copyright © 2016 GCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
